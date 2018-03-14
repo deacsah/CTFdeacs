26d2c797f21be2c53428ff7e7b995b8ad099a836
deacsah <mdeacs87@gmail.com>
2018-03-14 10:11:10 +0000

    Update answer.py
    Added extra chars for the brute forcing

--- Bitwise/answer.py
+++ Bitwise/answer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-user_submitted = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+{}|\';?/.,><\""
+user_submitted = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_+{}[]|/\':;?/.,><`~\""
 
 verify_arr = [193, 35, 9, 33, 1, 9, 3, 33, 9, 225]
 
